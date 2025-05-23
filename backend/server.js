ac5088f7e954cd126601b2edc36e61507d122908
Anish-Dangol <anis.dangol121@gmail.com>
2025-05-23 11:00:20 +0545

    3 - Payload Limit Updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());    // To parse req.body bodies
+app.use(express.json({limit: "5mb"}));    // To parse req.body bodies
+// limit shouldn't be too high to prevent DOS attacks
 app.use(express.urlencoded({ extended: true })); // To parse URL-encoded bodies || to parse form data(urlencoded)
 
 app.use(cookieParser());
